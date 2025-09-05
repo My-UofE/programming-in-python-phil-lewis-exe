222a32948ee0d2384cfcb12e320b9a635d5c36a7
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-09-05 15:16:46 +0000

    fix

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -4,7 +4,7 @@ for PROFILE in "/workspaces/*/.profile"; do
 echo RUNNING SETUP for $REPO
 if [ -f $PROFILE/bashrc ]; then
    echo SETTING UP BASHRC
-   mv ~/.bashrc ~/.bashrc.bk
+   mv ~/.bashrc ~/.bashrc.bk || echo 
    mv ~/.bash_history ~/.bash_history.bk || echo no hist
    ln -s $PROFILE/bashrc ~/.bashrc || echo FAILED_LINK_BASHRC
    ln -s $PROFILE/.profile/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
